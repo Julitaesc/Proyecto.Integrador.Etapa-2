daefc428f3518ea1dfddf00cec4733a6664851be
Juliana Escudero <julianaaescudero@gmail.com>
2023-10-25 17:16:24 -0300

    ultimando detallers

--- vite.config.js
+++ vite.config.js
@@ -14,10 +14,10 @@ build: {
         emptyOutDir: true,
         rollupOptions: {
             input: {
-            carrito: resolve("pages/carrito.html"),
-            contacto: resolve("pages/contacto.html"),
-            nosotros: resolve("pages/nosotros.html"),
-            inicio: resolve("index.html")
+            carrito: resolve('./pages/carrito.html'),
+            contacto: resolve('./pages/contacto.html'),
+            nosotros: resolve('./pages/nosotros.html'),
+            inicio: resolve('index.html')
 }
 }
 }
